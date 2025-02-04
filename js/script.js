68b95cc9e1e188ceeddfdb5125d6ee1c39cac008
purrperl <purrperl.dude@gmail.com>
2025-02-04 12:03:36 -1000

    Take 3. Fixed some bugs. Add icons next.

--- js/script.js
+++ js/script.js
@@ -1,16 +1,16 @@
 $(document).ready(function() {
-    $('.collapsible .toggle-icon').on('click', function() {
+    $(document).on('click', '.expando::before', function() {
         const $icon = $(this);
         const $content = $icon.next('.content');
 
         if ($content.hasClass('collapsed')) {
             $content.removeClass('collapsed');
-            $icon.text('-');
-            $content.find('.content').removeClass('collapsed').prev('.toggle-icon').text('-');
+            $icon.attr('src', 'icons/collapse.png');
+            $content.find('.content').removeClass('collapsed').prev('.toggle-icon').attr('src', 'icons/collapse.png');
         } else {
             $content.addClass('collapsed');
-            $icon.text('+');
-            $content.find('.content').addClass('collapsed').prev('.toggle-icon').text('+');
+            $icon.attr('src', 'icons/expand.png');
+            $content.find('.content').addClass('collapsed').prev('.toggle-icon').attr('src', 'icons/expand.png');
         }
     });
 });
