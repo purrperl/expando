a5e53b549bcb5aa750da54124f5d788beb4f243a
purrperl <purrperl.dude@gmail.com>
2025-02-05 10:40:33 -1000

    Add icons in CSS. Untested.

--- js/script.js
+++ js/script.js
@@ -1,22 +1,4 @@
-// Function to get/set cssVar 
-const cssVar = (name, value) => {
-    if (name.substr(0, 2) !== "--") {
-        name = "--" + name;
-    }
-
-    if (value) {
-        document.documentElement.style.setProperty(name, value)
-    }
-
-    return getComputedStyle(document.documentElement).getPropertyValue(name);
-}
-
-let collapse_icon = cssVar('--collapse-icon');
-let expand_icon = cssVar('--expand-icon');
-
-// Wait for the document to be fully loaded
 document.addEventListener('DOMContentLoaded', function() {
-    // Add click event listener to the toggle icon
     const expandoIcons = document.querySelectorAll('.expando');
     expandoIcons.forEach(icon => {
         icon.addEventListener('click', function() {
@@ -26,11 +8,11 @@ document.addEventListener('DOMContentLoaded', function() {
             // Toggle the collapsed class on the content element
             if (isCollapsed) {
                 content.classList.remove('collapsed');
-                icon.style.backgroundImage = `url(${collapse_icon})`;
+                content.classList.add('expanded');
                 content.style.maxHeight = content.scrollHeight + 'px';
             } else {
                 content.classList.add('collapsed');
-                icon.style.backgroundImage = `url(${expand_icon})`;
+                content.classList.remove('expanded');
                 content.style.maxHeight = '0px';
             }
         });
