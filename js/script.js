49154bca602aecd4f333c0be1bb5ea20db358987
purrperl <purrperl.dude@gmail.com>
2025-02-05 10:31:55 -1000

    Revert to vanilla JS. Untested.

--- js/script.js
+++ js/script.js
@@ -1,42 +1,38 @@
-
-
 // Function to get/set cssVar 
-const cssVar = ( name, value ) => {
-    if(name.substr(0, 2) !== "--") {
+const cssVar = (name, value) => {
+    if (name.substr(0, 2) !== "--") {
         name = "--" + name;
     }
 
-    if(value) {
+    if (value) {
         document.documentElement.style.setProperty(name, value)
     }
 
     return getComputedStyle(document.documentElement).getPropertyValue(name);
 }
 
-let collapse_icon = cssVar(--collapse-icon);
-let expand_icon = cssVar(--expand-icon);
-
+let collapse_icon = cssVar('--collapse-icon');
+let expand_icon = cssVar('--expand-icon');
 
 // Wait for the document to be fully loaded
 document.addEventListener('DOMContentLoaded', function() {
     // Add click event listener to the toggle icon
-    const expandoIcons = document.querySelectorAll('.expando::before');
-    for (let i = 0; i < expandoIcons.length; i++) {
-        expandoIcons[i].addEventListener('click', function() {
-            const icon = this;
+    const expandoIcons = document.querySelectorAll('.expando');
+    expandoIcons.forEach(icon => {
+        icon.addEventListener('click', function() {
             const content = icon.parentNode.querySelector('.content');
             const isCollapsed = content.classList.contains('collapsed');
 
             // Toggle the collapsed class on the content element
             if (isCollapsed) {
                 content.classList.remove('collapsed');
-                icon.style.backgroundImage = url(collapse_icon);
-                content.style.maxHeight = content.offsetHeight + 'px';
+                icon.style.backgroundImage = `url(${collapse_icon})`;
+                content.style.maxHeight = content.scrollHeight + 'px';
             } else {
                 content.classList.add('collapsed');
-                icon.style.backgroundImage = url(expand_icon);
+                icon.style.backgroundImage = `url(${expand_icon})`;
                 content.style.maxHeight = '0px';
             }
         });
-    }
+    });
 });
