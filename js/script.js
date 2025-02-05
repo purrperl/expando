549ced32480258e8cefa68c1c77e339ab8a6ba66
purrperl <purrperl.dude@gmail.com>
2025-02-05 11:40:34 -1000

    Add data-expanded attribute. Untested.

--- js/script.js
+++ js/script.js
@@ -1,14 +1,22 @@
 document.addEventListener('DOMContentLoaded', function() {
     const defaultIconSize = '20px';
-
     const iconSize = document.body.getAttribute('data-icon-size') || defaultIconSize;
-
     document.documentElement.style.setProperty('--icon-size', iconSize);
 
     const expandoIcons = document.querySelectorAll('.expando');
     expandoIcons.forEach(icon => {
+        const initialState = icon.getAttribute('data-expanded') === 'true';
+        const content = icon.parentNode.querySelector('.content');
+
+        if (initialState) {
+            content.classList.add('expanded');
+            content.style.maxHeight = content.scrollHeight + 'px';
+        } else {
+            content.classList.add('collapsed');
+            content.style.maxHeight = '0px';
+        }
+
         icon.addEventListener('click', function() {
-            const content = icon.parentNode.querySelector('.content');
             const isCollapsed = content.classList.contains('collapsed');
 
             if (isCollapsed) {
